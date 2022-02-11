6bac530359c48156e64e3d11cfd11bef71643ccd
yankee624 <kichang96@snu.ac.kr>
2022-02-11 16:37:32 +0900

    Add gitignore

--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,3 @@
 rm ex
-g++ -std=c++2a -lstdc++fs -o ex ex.cpp -I./include  /opt/libjpeg-turbo/lib64/libjpeg.a
+g++ -std=c++2a -lstdc++fs -o ex.out ex.cpp -I./include  /opt/libjpeg-turbo/lib64/libjpeg.a
 ./ex
